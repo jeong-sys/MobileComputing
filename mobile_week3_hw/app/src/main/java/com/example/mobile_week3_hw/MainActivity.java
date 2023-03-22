0066aac0ae1c2f3c77d55c3899cc3dd5c7d36734
jeong-sys <20211939@edu.hanbat.ac.kr>
2023-03-22 18:59:49 +0900

    19:00

--- mobile_week3_hw/app/src/main/java/com/example/mobile_week3_hw/MainActivity.java
+++ mobile_week3_hw/app/src/main/java/com/example/mobile_week3_hw/MainActivity.java
@@ -26,34 +26,44 @@ public class MainActivity extends AppCompatActivity {
 
             switch(view.getId()){
                 case R.id.num_0:
-                    textView.setText(current + "0");
+                    //textView.setText(current + "0");
+                    textView.append("0");
                     break;
                 case R.id.num_1:
-                    textView.setText(current + "1");
+                    //textView.setText(current + "1");
+                    textView.append("1");
                     break;
                 case R.id.num_2:
-                    textView.setText(current + "2");
+                    // textView.setText(current + "2");
+                    textView.append("2");
                     break;
                 case R.id.num_3:
-                    textView.setText(current + "3");
+                    // textView.setText(current + "3");
+                    textView.append("3");
                     break;
                 case R.id.num_4:
-                    textView.setText(current + "4");
+                    // textView.setText(current + "4");
+                    textView.append("4");
                     break;
                 case R.id.num_5:
-                    textView.setText(current + "5");
+                    // textView.setText(current + "5");
+                    textView.append("5");
                     break;
                 case R.id.num_6:
-                    textView.setText(current + "6");
+                    // textView.setText(current + "6");
+                    textView.append("6");
                     break;
                 case R.id.num_7:
-                    textView.setText(current + "7");
+                    // textView.setText(current + "7");
+                    textView.append("7");
                     break;
                 case R.id.num_8:
-                    textView.setText(current + "8");
+                    // textView.setText(current + "8");
+                    textView.append("8");
                     break;
                 case R.id.num_9:
-                    textView.setText(current + "9");
+                    // textView.setText(current + "9");
+                    textView.append("9");
                     break;
 
                 case R.id.plus:
@@ -63,11 +73,13 @@ public class MainActivity extends AppCompatActivity {
                     else{
                         stored = Double.parseDouble(current);
                         curOperator = '+';
-                        textView.setText("");
+                        //textView.setText("");
+                        textView.append(" + ");
                         break;
                     }
 
                 case R.id.result:
+
                     if(current.equals("")){
                         Toast.makeText(getApplicationContext(), "숫자를 먼저 입력하세요", Toast.LENGTH_SHORT).show();
                     }
@@ -80,8 +92,11 @@ public class MainActivity extends AppCompatActivity {
                                 result_val = stored + thisValue;
                                 break;
                     }
-                    textView.setText("" + result_val);
-                    stored = 0.0;
+                    // textView.setText("" + result_val);
+                    String result = String.valueOf(result_val);
+                    textView.append(result);
+
+                    // stored = 0.0;
                     break;
             }
         }
