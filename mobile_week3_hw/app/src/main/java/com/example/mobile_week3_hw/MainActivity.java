db554eb9efa4d8c908594e304df5b32063cf9106
you <20211939@edu.hanbat.ac.kr>
2023-03-23 13:02:30 +0900

    13:02

--- mobile_week3_hw/app/src/main/java/com/example/mobile_week3_hw/MainActivity.java
+++ mobile_week3_hw/app/src/main/java/com/example/mobile_week3_hw/MainActivity.java
@@ -1,17 +1,25 @@
 package com.example.mobile_week3_hw;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.viewmodel.CreationExtras;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.DuplicateFormatFlagsException;
+
 public class MainActivity extends AppCompatActivity {
 
     TextView textView;
     double stored;
-    char curOperator;
+    String curOperator;
+
+    String val;
+    double val_plus;
+    String val_2;
+    String val_3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,69 +35,141 @@ public class MainActivity extends AppCompatActivity {
         switch(view.getId()){
             case R.id.num_0:
                 textView.setText(current + "0");
+                val = current + "0";
                 break;
+
             case R.id.num_1:
                 textView.setText(current + "1");
+                val = current + "1";
                 break;
+
             case R.id.num_2:
                 textView.setText(current + "2");
+                val = current + "2";
                 break;
+
             case R.id.num_3:
                 textView.setText(current + "3");
+                val = current + "3";
                 break;
+
             case R.id.num_4:
                 textView.setText(current + "4");
+                val = current + "4";
                 break;
+
             case R.id.num_5:
                 textView.setText(current + "5");
-
+                val = current + "5";
                 break;
+
             case R.id.num_6:
                 textView.setText(current + "6");
-
+                val = current + "6";
                 break;
+
             case R.id.num_7:
                 textView.setText(current + "7");
-
+                val = current + "7";
                 break;
+
             case R.id.num_8:
                 textView.setText(current + "8");
-
+                val = current + "8";
                 break;
+
             case R.id.num_9:
                 textView.setText(current + "9");
-
+                val = current + "9";
                 break;
 
             case R.id.plus:
                 if(current.equals("")){
-                    Toast.makeText(getApplicationContext(), "숫자를 먼저 입력하세요", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "숫자를 입력주세요", Toast.LENGTH_SHORT).show();
                 }
                 else{
                     stored = Double.parseDouble(current);
-                    curOperator = '+';
+                    curOperator = "+";
                     textView.setText("");
+                    val_2 = val + " + ";
                     break;
                 }
 
+            case R.id.minus:
+                if(current.equals("")){
+                    Toast.makeText(getApplicationContext(), "숫자를 입력주세요", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    stored = Double.parseDouble(current);
+                    curOperator = "-";
+                    textView.setText("");
+                    val_2 = val + " - ";
+                    break;
+                }
+
+            case R.id.mul:
+                if(current.equals("")){
+                    Toast.makeText(getApplicationContext(), "숫자를 입력주세요", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    stored = Double.parseDouble(current);
+                    curOperator = "*";
+                    textView.setText("");
+                    val_2 = val + " * ";
+                    break;
+                }
+
+            case R.id.div:
+                if(current.equals("")){
+                    Toast.makeText(getApplicationContext(), "숫자를 입력주세요", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    stored = Double.parseDouble(current);
+                    curOperator = "/";
+                    textView.setText("");
+                    val_2 = val + " / ";
+                    break;
+                }
+
+            case R.id.clear:
+                textView.setText("");
+                stored = 0;
+                break;
+
             case R.id.result:
 
+                // 숫자를 입력하지 않고 연산자나 = 누르는 경우
                 if(current.equals("")){
-                    Toast.makeText(getApplicationContext(), "숫자를 먼저 입력하세요", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "숫자를 입력주세요", Toast.LENGTH_SHORT).show();
                 }
+
                 else{
                     double result_val = 0;
                     double thisValue = Double.parseDouble(textView.getText().toString());
 
                     switch(curOperator){
-                        case '+':
+
+                        case "+":
                             result_val = stored + thisValue;
+                            val_3 = val_2 + val + " = " + result_val;
+                            break;
+
+                        case "-":
+                            result_val = stored - thisValue;
+                            val_3 = val_2 + val + " = " + result_val;
+                            break;
+
+                        case "*":
+                            result_val = stored * thisValue;
+                            val_3 = val_2 + val + " = " + result_val;
+                            break;
+
+                        case "/":
+                            result_val = stored / thisValue;
+                            val_3 = val_2 + val + " = " + result_val;
                             break;
                     }
-                    // textView.setText("" + result_val)
-                    String a = .getText().toString();
-                    textView.setText(textView.getText().toString()+a);
-
+                    textView.setText(val_3);
 
                     stored = 0.0;
                     break;
