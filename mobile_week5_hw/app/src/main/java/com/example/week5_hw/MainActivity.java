058c9d4600a9a18ebb2331a8f29b1308f2f3f58b
jeong-sys <20211939@edu.hanbat.ac.kr>
2023-04-05 10:32:51 +0900

    10:32

--- mobile_week5_hw/app/src/main/java/com/example/week5_hw/MainActivity.java
+++ mobile_week5_hw/app/src/main/java/com/example/week5_hw/MainActivity.java
@@ -2,13 +2,50 @@ package com.example.week5_hw;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextClock;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        ViewGroup.LayoutParams params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT);
+
+        LinearLayout baseLayout = new LinearLayout(this);
+        baseLayout.setOrientation(LinearLayout.VERTICAL);
+        setContentView(baseLayout,params);
+
+        EditText editText = new EditText(this);
+        baseLayout.addView(editText);
+
+        Button btn = new Button(this);
+        btn.setText("버튼입니다");
+        btn.setBackgroundColor(Color.YELLOW);
+        baseLayout.addView(btn);
+
+        TextView textView = new TextView(this);
+        textView.setTextColor(Color.MAGENTA);
+        textView.setTextSize(20);
+        baseLayout.addView(textView);
+
+        btn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                String str;
+                str = editText.getText().toString();
+                textView.setText(str);
+            }
+        });
+
     }
 }
