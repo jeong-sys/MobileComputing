8b9d645e7e3c9d3f24ee9f3dfc04286ce695d80b
you <20211939@edu.hanbat.ac.kr>
2023-04-17 12:00:05 +0900

    11:59

--- mobile_week7_hw/app/src/main/java/com/example/mobile_week7_hw/MainActivity.java
+++ mobile_week7_hw/app/src/main/java/com/example/mobile_week7_hw/MainActivity.java
@@ -1,19 +1,21 @@
 package com.example.mobile_week7_hw;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
 import android.widget.EditText;
-import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
-import android.widget.Toolbar;
+import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -25,108 +27,110 @@ import com.example.mobile_week7_hw.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    private NavigationView navigationView;
-    private TextView headerName, headerEmail;
-    private ImageView headerImage;
-    private String selectedImage = "개";
+    private AppBarConfiguration mAppBarConfiguration;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-
-        navigationView = findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+        setSupportActionBar(binding.appBarMain.toolbar);
+        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                int id = item.getItemId();
-
-                if (id == R.id.nav_slideshow) {
-                    // do something
-                } else if (id == R.id.nav_logout) {
-                    logout();
-                }
-
-                DrawerLayout drawer = findViewById(R.id.drawer_layout);
-                drawer.closeDrawer(GravityCompat.START);
-                return true;
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
         });
-
-        View headerView = navigationView.getHeaderView(0);
-        headerName = headerView.findViewById(R.id.header_name);
-        headerEmail = headerView.findViewById(R.id.header_email);
-        headerImage = headerView.findViewById(R.id.header_image);
-
-        // Set default header info
-        headerName.setText("이름");
-        headerEmail.setText("이메일");
-        headerImage.setImageResource(R.drawable.dog);
-
-    }
-
-    private void setSupportActionBar(Toolbar toolbar) {
-
+        DrawerLayout drawer = binding.drawerLayout;
+        NavigationView navigationView = binding.navView;
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                .setOpenableLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+
+    @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
-        if (id == R.id.action_login) {
-            showLoginDialog();
-            return true;
+
+        if (id == R.id.login){
+            show_alertdialog();
         }
         return super.onOptionsItemSelected(item);
     }
 
-    private void showLoginDialog() {
-        LayoutInflater inflater = LayoutInflater.from(this);
-        View dialogView = inflater.inflate(R.layout.dialog_login, null);
+    private void show_alertdialog(){
 
-        final EditText nameEditText = dialogView.findViewById(R.id.edit_text_name);
-        final EditText emailEditText = dialogView.findViewById(R.id.edit_text_email);
-        final RadioGroup radioGroup = dialogView.findViewById(R.id.radio_group);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("사용자 입력");
+        builder.setIcon(R.mipmap.ic_launcher);
 
-        AlertDialog.Builder builder = new AlertDialog.Builder(this)
-                .setTitle("로그인")
-                .setView(dialogView)
-                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        String name = nameEditText.getText().toString();
-                        String email = emailEditText.getText().toString();
+        // set the custom layout
+        View alertdialog = getLayoutInflater().inflate(R.layout.alertdialog, null);
+        builder.setView(alertdialog);
 
-                        // Get selected radio button
-                        int selectedId = radioGroup.getCheckedRadioButtonId();
-                        RadioButton radioButton = dialogView.findViewById(selectedId);
-                        selectedImage = radioButton.getText().toString();
+        // get references to the views in the custom layout
+        EditText editName = alertdialog.findViewById(R.id.editname);
+        EditText editMail = alertdialog.findViewById(R.id.editmail);
+        RadioGroup radioGroup = alertdialog.findViewById(R.id.radio_group);
 
-                        // Update header info
-                        headerName.setText(name);
-                        headerEmail.setText(email);
-                        if (selectedImage.equals("개")) {
-                            headerImage.setImageResource(R.drawable.dog);
-                        } else if (selectedImage.equals("고양이")) {
-                            headerImage.setImageResource(R.drawable.cat);
-                        } else if (selectedImage.equals("말")) {
-                            headerImage.setImageResource(R.drawable.horse);
-                        }
-                    }
-                })
-                .setNegativeButton("Cancel", null);
+        // set up the radio group
+        RadioButton dog = alertdialog.findViewById(R.id.dog);
+        RadioButton cat = alertdialog.findViewById(R.id.cat);
+        RadioButton horse = alertdialog.findViewById(R.id.horse);
 
+        // set the positive and negative buttons
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String name = editName.getText().toString();
+                String email = editMail.getText().toString();
+                int selectedId = radioGroup.getCheckedRadioButtonId();
+                String photoType = "";
+
+                if (selectedId == dog.getId()) {
+                    photoType = "Photo 1";
+                } else if (selectedId == cat.getId()) {
+                    photoType = "Photo 2";
+                } else if (selectedId == horse.getId()) {
+                    photoType = "Photo 3";
+                }
+
+                Toast.makeText(MainActivity.this, "Name: " + name + ", Email: " + email + ", Photo Type: " + photoType, Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+    }
+}
