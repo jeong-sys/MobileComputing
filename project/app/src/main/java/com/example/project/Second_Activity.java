af64694107562594e322af6bd1fbbc9813901792
you <20211939@edu.hanbat.ac.kr>
2023-05-21 01:14:54 +0900

    1:14

---
+++ project/app/src/main/java/com/example/project/Second_Activity.java
@@ -0,0 +1,15 @@
+package com.example.project;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Second_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_linear);
+
+    }
+
+}
