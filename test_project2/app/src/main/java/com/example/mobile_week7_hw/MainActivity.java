8b32b23f3275eba33a1ee23b348efea85c7f0a3d
you <20211939@edu.hanbat.ac.kr>
2023-05-21 16:09:10 +0900

    16:08

---
+++ test_project2/app/src/main/java/com/example/mobile_week7_hw/MainActivity.java
@@ -0,0 +1,92 @@
+package com.example.mobile_week7_hw;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
+public class MainActivity extends AppCompatActivity {
+    int RC_SIGN_IN = 1000;
+
+    GoogleSignInClient mGoogleSignInClient;
+    SignInButton mSignInButton;
+    // Button mSignOutButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.linear);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail().build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        mSignInButton = findViewById(R.id.sign_in_button);
+        // mSignOutButton = findViewById(R.id.sign_out_button);
+
+        mSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
+
+//        mSignOutButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                signOut();
+//            }
+//        });
+    }
+
+    public void signIn(){
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+//    public void signOut(){
+//        mGoogleSignInClient.signOut()
+//                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<Void> task) {
+//                        updateUI(null);
+//                    }
+//                    private void updateUI(Object o) {
+//                    }
+//                });
+//    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent Data) {
+        super.onActivityResult(requestCode, resultCode, Data);
+
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(Data);
+            try {
+                // 로그인 성공
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                // 새로운 액티비티로 이동
+                Intent intent = new Intent(MainActivity.this, Second_activity.class);
+                startActivity(intent);
+
+            } catch (ApiException e) {
+                // 로그인 실패
+                e.printStackTrace();
+                String msg = "로그인에 실패하였습니다";
+                Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+            }
+        }
+
+    }
+}
